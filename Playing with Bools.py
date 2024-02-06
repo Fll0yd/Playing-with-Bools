35318c8fb1287ab7ec8b6011bc8d9d5d250b6b4a
Kenneth <kenlloydb@gmail.com>
2024-02-06 12:14:07 -0600

    Initial commit

---
+++ Playing with Bools.py
@@ -0,0 +1,11 @@
+me = "Flloyd"
+print (me)
+me == "Flloyd"
+print(type(me == "Flloyd"))
+me == "Nick"
+not me == "Nick"
+name = input("What is your name?\n")
+if name != "Ben":
+    print ("Get out!")
+else:
+    print ("Come on in!")
